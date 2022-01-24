ebac16f03c19c7a11ed2468a902207334af6b349
Latif <LAbu@spartaglobal.com>
2022-01-24 12:56:28 +0000

    data type exercises

--- 01_variables/Data types.py
+++ 01_variables/Data types.py
@@ -1,5 +1,79 @@
-i = 375 # int
-f = 3.75 # float
-c = 3j +2 # complex number
+# i = 375 # int
+# f = 3.75 # float
+# c = 3j +2 # complex number
+#
+# print(c, type(c))
+# add = 3+5
+# subtract = 3-5
+# multiply = 3 * 5
+# divide = 3 / 5
+# power = 3**5
+# modula = 3 % 5
+# print (13 // 3) # makes the number a whole number, to the lowest nunber
+# # or
+# print(14 // 3, 14 % 3)
 
-print(c, type(c))
+# single = "string in single quotes"
+# double = "string in double quotes"
+# both = "this is Latif's \"string\""
+#
+#
+# # indexing and slicing
+# hi = "Hello World!"
+# print(hi[6]) #w
+# print(hi[0:6]) # includes first upto the assinged
+# print(hi[3:8])
+# print(len(hi))
+#
+# # methods
+#
+# white_space = "   lots of white space   "
+# print(len(white_space))
+# print(white_space.strip()) # no white spaces at either end now
+# print(white_space.rstrip()) # strips from the left and not the right
+# print(white_space.upper())## makes string upper case
+# print(white_space.strip().capitalize()) # capitalise alone will not capitalise the variable but . strip first will
+# print(white_space(" "))
+# print(white_space.strip().count(" "))# will count the number of spaces
+# print(white_space.replace"o", "ooooo".replace("i", "ooo"))
+#
+# print(white_space)
+# ## f-strings (formatted strigns)
+# pi= 3.14159265959
+# print(f"Pi to 3dp:{pi:3f}")
+#
+# score = 16
+# max_score =26
+# print(f"you scored {score / max_score}")
+# print(f"you scored {score / max_score: .2f}")# 2 decimal palces
+# print(f"you scored {score / max_score:%}") # percentage
+# print(f"you scored {score / max_score: .2%}")# percentage plus 2 decimal places
+
+#boolean
+
+# t = True
+# f = False
+# print(t, type(t))
+# print(t, type(t))
+#
+# print(3+2 == 5)
+# # print(5 != 5 )
+# print (True and True)
+
+# age = 19
+# drink = "alcohol"
+# print(age > 18 and drink == alcohol)
+
+hi = " Hello World"
+print(hi.isalpha())
+print(hi.islower())
+print(hi.isupper())
+#print(hi.endswith())
+#print(hi.startswith())
+print(hi.strip(" World").isalpha())
+print(hi.lower().islower())
+print(hi.upper().isupper())
+
+
+# and if the conditions have numbers, what happens?#
+

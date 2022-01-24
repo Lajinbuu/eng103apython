e1e025eca80a3f8f78e984fcf109c6f42a67cf6f
Latif <LAbu@spartaglobal.com>
2022-01-24 11:41:13 +0000

    date type

--- 01_variables/02_input.py
+++ 01_variables/02_input.py
@@ -2,5 +2,6 @@ name = input(" What is your name?\n ")
 age = input("What is your age \n ")
 favourite_book = input("Name your favourite book \n")
 
+# an f strong is a formatted string uses curly brackets
 
 
---
+++ 01_variables/Data types.py
@@ -0,0 +1,5 @@
+i = 375 # int
+f = 3.75 # float
+c = 3j +2 # complex number
+
+print(c, type(c))

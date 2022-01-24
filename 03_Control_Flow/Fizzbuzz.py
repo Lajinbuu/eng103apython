cfc0321b6c4f23440cec2a6f928f449ccfb4595a
Latif <LAbu@spartaglobal.com>
2022-01-24 17:29:05 +0000

    Cleaned up code

--- 03_Control_Flow/Fizzbuzz.py
+++ 03_Control_Flow/Fizzbuzz.py
@@ -1,17 +1,17 @@
-#Basic version
+# Basic version
 # for numbers 1 to 100
 # play fizzbuzz
-#print the number
+# print the number
 # if the divisible by 3 fizz
 # if divisible by 5 buzz
 # if both fizzbuzz
 
 
-#what can you add?
-#can we customise the end number
-#or the start number#
-#can we get those from player input
-#can we input alternate words for fizz and buzz?
+# what can you add?
+# can we customise the end number
+# or the start number#
+# can we get those from player input
+# can we input alternate words for fizz and buzz?
 
 # for i in range(1,101):
 #     if i % 3 == 0 and i % 5 == 0:
@@ -25,27 +25,28 @@
 
 
 # number = numbers[0:101], but have to inout
-start_number = int(input("Enter a start number")) # made variable which represents the starting number, user will enter a starting number in the console
-end_number = int(input("Enter an end number"))  # made variable which represents the ending number, user will enter a starting number in the console
-phrase = input("Enter any word") # first phrase
-phrase_2 = input("Enter another word") # second phrase
-phrase_3 = input("Enter another word")# thrid phrase
-for i in range(start_number,end_number):
+start_number = int(input(
+    "Enter a start number"))  # made variable which represents the starting number, user will enter a starting number in the console
+end_number = int(input(
+    "Enter an end number"))  # made variable which represents the ending number, user will enter a starting number in the console
+phrase = input("Enter any word")  # first phrase
+phrase_2 = input("Enter another word")  # second phrase
+phrase_3 = input("Enter another word")  # thrid phrase
+for i in range(start_number, end_number):
     if i % 3 == 0 and i % 5 == 0:  # checks if number is divisible by 3 and 5
         print(phrase)
-    elif i % 3 == 0:          # checks if number is divisible by 3
+    elif i % 3 == 0:  # checks if number is divisible by 3
         print(phrase_2)
-    elif i % 5 == 0: # checks if number is divisible by 5
+    elif i % 5 == 0:  # checks if number is divisible by 5
         print(phrase_3)
-    elif i % 2 == 0: # checks if number is even
+    elif i % 2 == 0:  # checks if number is even
         print("Even")
     else:
         print(i)
 
-
 ### if the player wanted to enter their own number they can follwo the steps bellow
 
-#int(input("Enter a number between 1-100"))
+# int(input("Enter a number between 1-100"))
 
 # player_number = input("Enter a number")
 # if int(player_number) % 3 == 0 and int(player_number) % 5 == 0:
@@ -54,13 +55,3 @@ for i in range(start_number,end_number):
 #     print("fizz")
 # elif int (player_number) % 5 == 0:
 #     print("buzz")
-
-
-
-
-
-
-
-
-
-
